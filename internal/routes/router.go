941f70337728a6a90ad2e46cf0baad4ffd47db45
jdiaconu <jean.diaconu@gmail.com>
2021-05-31 17:03:34 +0200

    [Metrics] Add Gorilla Mux Metrics Bundle

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -14,12 +14,34 @@ import (
 
 	"github.com/bit-broker/rate-service/internal/controllers"
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// Metrics for http duration
+var (
+	httpDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "myapp_http_duration_seconds",
+		Help: "Duration of HTTP requests.",
+	}, []string{"path"})
 )
 
 // CheckAPI : Returns API up
 func CheckAPI(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 	_, _ = w.Write([]byte("Rate Service API v1 Up & Running"))
+}
+
+// Prometheus Middleware
+func prometheusMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		route := mux.CurrentRoute(r)
+		path, _ := route.GetPathTemplate()
+		timer := prometheus.NewTimer(httpDuration.WithLabelValues(path))
+		next.ServeHTTP(w, r)
+		timer.ObserveDuration()
+	})
 }
 
 // InitializeRouter : Initializes the router
@@ -34,6 +56,10 @@ func InitializeRouter() *mux.Router {
 	router.Handle("/api/v1/{uid}/config", http.HandlerFunc(controllers.GetConfig)).Methods("GET")
 	router.Handle("/api/v1/{uid}/config", http.HandlerFunc(controllers.CreateOrUpdateConfig)).Methods("PUT")
 	router.Handle("/api/v1/{uid}/config", http.HandlerFunc(controllers.DeleteConfig)).Methods("DELETE")
+
+	// Metrics
+	router.Use(prometheusMiddleware)
+	router.Path("/metrics").Handler(promhttp.Handler())
 
 	return router
 }
