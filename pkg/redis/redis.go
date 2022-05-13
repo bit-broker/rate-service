0176f52c2ff60ccde6ccfa12361f4f0ba2751ef2
James Walker <jawalke2@cisco.com>
2022-05-13 12:02:13 +0100

    Added LICENSE file to repo, added / updated license headers to source files

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -1,17 +1,31 @@
+// Copyright 2021 Cisco and its affiliates
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 /*
  * File Name : mongo.go
  * Creation Date : 21-12-2020
  * Written by : Jean Diaconu <jdiaconu@cisco.com>
- * Copyright (C) Cisco System Inc - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
  */
 
 package redis
 
 import (
-	"github.com/bit-broker/rate-service/internal/helper"
 	"strconv"
+
+	"github.com/bit-broker/rate-service/internal/helper"
 
 	"github.com/alicebob/miniredis"
 	"github.com/go-redis/redis/v8"
